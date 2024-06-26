ed4d3c455200301f1fd47f5725008722dd2a8746
Sohei Shirakawa <sohei@indeed.com>
2024-06-26 13:48:40 +0900

    Initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "https://final-capstone-welovemovies-354g.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
